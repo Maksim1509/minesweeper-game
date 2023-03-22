73fc4b8360cd3c6c0cfd19090042be160e4ab982
Maksim1509 <maaa23@mail.ru>
2023-03-22 23:22:26 +0400

    refactor: use clsx in header field component

--- src/components/Field/HeaderField/HeaderField.tsx
+++ src/components/Field/HeaderField/HeaderField.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 import { useAppDispatch, useAppSelector } from '../../../hooks/hooks';
 import './HeaderField.scss';
 import { restart, Status } from '../../../store/gameSlice';
@@ -9,16 +10,14 @@ const HeaderField = () => {
   const handleRestart = () => {
     dispatch(restart());
   };
-
+  const iconClass = {
+    ['header-field__icon']: true,
+    ['header-field__icon_lose']: status === Status.lose,
+    ['header-field__icon_win']: status === Status.win,
+  };
   return (
     <section className="header-field">
-      <div
-        className={`header-field__icon ${
-          status === Status.lose ? 'header-field__icon_lose' : ''
-        }
-        ${status === Status.win ? 'header-field__icon_win' : ''}`}
-        onClick={handleRestart}
-      ></div>
+      <div className={clsx(iconClass)} onClick={handleRestart}></div>
     </section>
   );
 };
